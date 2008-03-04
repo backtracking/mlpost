f4b642fac32ce28b4a647b0d53291f9a5bee02de
Jean-Christophe Filliatre <filliatr@lri.fr>
2008-03-04 14:13:45 +0000

    headers + license

--- helpers.ml
+++ helpers.ml
@@ -1,3 +1,19 @@
+(**************************************************************************)
+(*                                                                        *)
+(*  Copyright (C) Johannes Kanig, Stephane Lescuyer                       *)
+(*  and Jean-Christophe Filliatre                                         *)
+(*                                                                        *)
+(*  This software is free software; you can redistribute it and/or        *)
+(*  modify it under the terms of the GNU Library General Public           *)
+(*  License version 2, with the special exception on linking              *)
+(*  described in file LICENSE.                                            *)
+(*                                                                        *)
+(*  This software is distributed in the hope that it will be useful,      *)
+(*  but WITHOUT ANY WARRANTY; without even the implied warranty of        *)
+(*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                  *)
+(*                                                                        *)
+(**************************************************************************)
+
 open Path
 open Point
 open Num
