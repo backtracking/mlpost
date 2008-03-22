9953419891cce9846427d67bf04302f22e7d7832
Johannes Kanig <johannes.kanig@lri.fr>
2008-03-22 21:18:15 +0000

    module F = Format -> open Format

--- generate.ml
+++ generate.ml
@@ -14,28 +14,28 @@
 (*                                                                        *)
 (**************************************************************************)
 
-module F = Format
+open Format
 
 let generate_tex tf tmpl1 tmpl2 l =
   let minipage fmt i tmpl =
-    F.fprintf fmt "@[<hov 2>\\begin{minipage}[tb]{0.5\\textwidth}@\n";
-    F.fprintf fmt "@[<hov 2>\\begin{center}@\n";
-    F.fprintf fmt 
+    fprintf fmt "@[<hov 2>\\begin{minipage}[tb]{0.5\\textwidth}@\n";
+    fprintf fmt "@[<hov 2>\\begin{center}@\n";
+    fprintf fmt 
       "\\includegraphics[width=\\textwidth,height=\\textwidth,keepaspectratio]{%s.%i}" 
       tmpl i;
-    F.fprintf fmt "@]@\n\\end{center}@\n";
-    F.fprintf fmt "@]@\n\\end{minipage}@\n"
+    fprintf fmt "@]@\n\\end{center}@\n";
+    fprintf fmt "@]@\n\\end{minipage}@\n"
   in
     Misc.write_to_formatted_file tf
       (fun fmt ->
-          F.fprintf fmt "\\documentclass[a4paper]{article}@.";
-          F.fprintf fmt "\\usepackage[]{graphicx}@.";
-          F.fprintf fmt "@[<hov 2>\\begin{document}@.";
+          fprintf fmt "\\documentclass[a4paper]{article}@.";
+          fprintf fmt "\\usepackage[]{graphicx}@.";
+          fprintf fmt "@[<hov 2>\\begin{document}@.";
           List.iter
             (fun (i,_) ->
-               F.fprintf fmt "@\n %i" i;
+               fprintf fmt "@\n %i" i;
                minipage fmt i tmpl1;
                minipage fmt i tmpl2;
-               F.fprintf fmt "@\n \\vspace{3cm}@\n"
+               fprintf fmt "@\n \\vspace{3cm}@\n"
             ) l ;
-          F.fprintf fmt "@]@\n\\end{document}@.")
+          fprintf fmt "@]@\n\\end{document}@.")
