4dd820a259a8074a0ecf195245869c616707cd60
Johannes Kanig <johannes.kanig@lri.fr>
2008-04-05 17:00:21 +0000

    elimine un bug embarassant dans Point.rotated rotatedaround pour les points un arbre aleatoire

--- point.ml
+++ point.ml
@@ -52,8 +52,10 @@ let rec add p1 p2 =
     | PTPair (a1,b1), PTPair (a2,b2) -> PTPair (a1 +. a2, b1 +. b2)
     | PTAdd (p1',p2'), _ -> add p1' (add p2' p2)
     | PTSub (p1',p2'), _ -> add p1' (sub p2 p2')
+(*
     | _, PTAdd (p1',p2') -> add p1' (add p1 p2')
     | _, PTSub (p1',p2') -> add p1' (sub p1 p2')
+      *)
     | _, _ -> PTAdd (p1,p2)
 
 and mult f p =
@@ -71,8 +73,10 @@ and sub p1 p2 =
     | PTPair (a1,b1), PTPair (a2,b2) -> PTPair (a1 -. a2, b1 -. b2)
     | PTAdd (p1',p2'), _ -> add p1' (sub p2' p2)
     | PTSub (p1',p2'), _ -> sub p1' (add p2' p2)
+(*
     | _, PTAdd (p1',p2') -> sub (sub p1 p1') p2'
     | _, PTSub (p1',p2') -> add (sub p1 p1') p2'
+      *)
     | _, _ -> PTSub (p1,p2)
 
 
@@ -81,12 +85,15 @@ let rec rotated f = function
   | PTPair (a,b) -> 
       let angle = Num.deg2rad f in
         PTPair (cos(angle) *. a -. sin(angle) *. b,
-               sin(angle) *. a -. cos(angle) *. b)
+               sin(angle) *. a +. cos(angle) *. b)
   | PTAdd (p1, p2) -> add (rotated f p1) (rotated f p2)
   | PTSub (p1, p2) -> sub (rotated f p1) (rotated f p2)
   | PTRotated (f', p) -> PTRotated (f+.f', p)
   | PTMult (f', p) -> PTMult(f', rotated f p)
   | _ as p -> PTRotated (f, p)
+
+(* rotate p2 around p1 *)
+let rotate_around p1 f p2 = add p1 (rotated f (sub p2 p1))
 
 let xscaled f = function
   | PTPair (a,b) -> PTPair (f *. a, b)
@@ -103,6 +110,7 @@ let transform tr p =
        | TRShifted p -> add acc p
        | TRRotated f -> rotated f acc
        | TRXscaled f -> rotated f acc
+       | TRRotateAround (p,f) -> rotate_around p f acc
        | _ as str -> simple_transform str acc
     ) p tr
     
