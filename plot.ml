3da619cc11b2f96357c40818fb228483af725921
Johannes Kanig <johannes.kanig@lri.fr>
2008-07-03 17:43:59 +0000

    plot en mode Num.t

--- plot.ml
+++ plot.ml
@@ -17,6 +17,8 @@
 open Command
 open Helpers
 open Path
+open Num
+open Num.Infix
 
 type skeleton = 
     {
@@ -37,11 +39,11 @@ let get_style = function
   | None -> fun i -> Dash.evenly, Pen.default ()
   | Some f -> f
 
-let off_pattern = fun i -> Dash.pattern [Dash.On 5.]
+let off_pattern = fun i -> Dash.pattern [Dash.On (f 5.)]
 let defpen = fun i -> Pen.default ()
 
-let get_borders sx sy h w = 0., sx *. (float_of_int w), 
-                            sy *. (float_of_int h), 0.
+let get_borders sx sy h w = f 0., sx */ (num_of_int w), 
+                            sy */ (num_of_int h), f 0.
 
 let draw_grid ?(hdash=off_pattern) ?(vdash=off_pattern) 
               ?(hpen=defpen) ?(vpen=defpen) 
@@ -49,13 +51,13 @@ let draw_grid ?(hdash=off_pattern) ?(vdash=off_pattern)
   let maxl, maxr, maxu, maxd = get_borders sx sy h w in
   let drawline dashed pen p = Command.draw ~pen ~dashed p in
   let horizontal i =
-    let y = float_of_int i *. sy in
-    let pi = path [maxl, y; maxr, y] in
+    let y = num_of_int i */ sy in
+    let pi = pathn [maxl, y; maxr, y] in
       drawline (hdash i) (hpen i) pi
   in
   let vertical i =
-    let x = float_of_int i *. sx in
-    let pi = path [x, maxd; x, maxu] in
+    let x = num_of_int i */ sx in
+    let pi = pathn [x, maxd; x, maxu] in
       drawline (vdash i) (vpen i) pi
   in
     seq (Misc.fold_from_to
@@ -65,9 +67,9 @@ let draw_grid ?(hdash=off_pattern) ?(vdash=off_pattern)
               [] 0 w) 0 h)
 
 let deflabel = Some (fun i -> Picture.tex (string_of_int i)) 
-let defticks = Some (0.25, Pen.default ())
+let defticks = Some ((f 0.25), Pen.default ())
 
-let get_corners maxu maxr = (0., maxu), (maxr, maxu), (0., 0.), (maxr, 0.)
+let get_corners maxu maxr = (f 0., maxu), (maxr, maxu), (f 0., f 0.), (maxr, f 0.)
 
 let draw_axes ?(hpen=Pen.default ()) ?(vpen=Pen.default ()) 
          ?(hlabel= deflabel) ?(vlabel=deflabel)
@@ -79,14 +81,14 @@ let draw_axes ?(hpen=Pen.default ()) ?(vpen=Pen.default ())
     | None -> Command.nop
     | Some labl -> 
 	Command.label ~pos:Plowleft labl 
-	  (Point.pt (float_of_int w *. sx, -.sy))
+	  (Point.pt (num_of_int w */ sx, f 0. -/sy))
   in
   let vcaptcmd = match vcaption with 
     | None -> Command.nop
     | Some labl -> 
 	Command.label ~pos:Plowleft 
 	  (Picture.transform [Transform.rotated 90.] labl)
-	  (Point.pt (-.sx, float_of_int h *. sy))
+	  (Point.pt (f 0. -/ sx, num_of_int h */ sy))
   in
   let labelcmd pos p i = function
     | None -> Command.nop
@@ -98,27 +100,27 @@ let draw_axes ?(hpen=Pen.default ()) ?(vpen=Pen.default ())
     | Some (f,pen) -> Command.draw ~pen (pathf f)
   in
   let horizontal i =
-    let x = float_of_int i *. sx in
-      seq [ labelcmd Pbot (Point.p (x,maxd)) i hlabel; 
-            ticks_cmd (fun f -> path [x,maxd; x, maxd +. sy *. f]);
+    let x = num_of_int i */ sx in
+      seq [ labelcmd Pbot (Point.pt (x,maxd)) i hlabel; 
+            ticks_cmd (fun f -> pathn [x,maxd; x, maxd +/ (sy */ f)]);
             if closed then
-              ticks_cmd (fun f -> path [x,maxu; x, maxu -. sy *.f])
+              ticks_cmd (fun f -> pathn [x,maxu; x, maxu -/ sy */f])
             else Command.nop ]
   in
   let vertical i =
-    let y = float_of_int i *. sy in
-      seq [labelcmd Pleft (Point.p (maxl, y)) i vlabel; 
-           ticks_cmd (fun f ->  path [maxl,y; maxl +. sx *. f,y]);
+    let y = num_of_int i */ sy in
+      seq [labelcmd Pleft (Point.pt (maxl, y)) i vlabel; 
+           ticks_cmd (fun f ->  pathn [maxl,y; maxl +/ sx */ f,y]);
             if closed then
-              ticks_cmd (fun f -> path [maxr,y; maxr -. sy *. f, y])
+              ticks_cmd (fun f -> pathn [maxr,y; maxr -/ sy */ f, y])
             else Command.nop ]
   in
     seq 
-      [Command.draw ~pen:hpen (path [ll; lr]);
-       Command.draw ~pen:vpen (path [ll; ul]);
+      [Command.draw ~pen:hpen (pathn [ll; lr]);
+       Command.draw ~pen:vpen (pathn [ll; ul]);
        if closed then
-         seq [Command.draw ~pen:hpen (path [ul; ur]);
-              Command.draw ~pen:vpen (path [lr; ur])]
+         seq [Command.draw ~pen:hpen (pathn [ul; ur]);
+              Command.draw ~pen:vpen (pathn [lr; ur])]
        else Command.nop;
        hcaptcmd; vcaptcmd;
        seq (Misc.fold_from_to
@@ -133,26 +135,26 @@ let draw_func ?(pen) ?(drawing=Normal) ?(style) ?(dashed) ?(label)
     f {width=w; height=h; stepx=sx; stepy=sy} =
   let maxl, maxr, maxu, maxd = get_borders sx sy h w in
   let ul, ur, ll, lr = get_corners maxu maxr in
-  let box = path ~style:JLine ~cycle:JLine [ul;ll;lr;ur] in
+  let box = pathn ~style:JLine ~cycle:JLine [ul;ll;lr;ur] in
   let normal acc i =
-    let x, y = (float_of_int i) *. sx, (f i) *. sy 
+    let x, y = (num_of_int i) */ sx, (Num.f (f i)) */ sy 
     in 
       (x,y)::acc
   in
   let stepwise (acc,x,y) i =
-    let nx, ny = (float_of_int i) *. sx, (f i) *. sy in
+    let nx, ny = (num_of_int i) */ sx, (Num.f (f i)) */ sy in
       (nx,ny) :: (nx,y) :: acc, nx, ny
   in
   let graph = 
     match drawing with
     | Normal -> Misc.fold_from_to normal [] 0 w
     | Stepwise -> 
-        let p, _,_ = Misc.fold_from_to stepwise ([],0.,0.) 0 w in p
+        let p, _,_ = Misc.fold_from_to stepwise ([],Num.f 0.,Num.f 0.) 0 w in p
   in
   let pic = Picture.clip 
-    (Picture.make (Command.draw ?pen ?dashed (path ?style graph))) box in 
+    (Picture.make (Command.draw ?pen ?dashed (pathn ?style graph))) box in 
     match label with 
       | None -> draw_pic pic 
       | Some (lab, pos, i) -> 
-	  let pt = Point.pt (float_of_int i *. sx, (f i) *. sy) in
+	  let pt = Point.pt (num_of_int i */ sx, (Num.f (f i)) */ sy) in
 	    seq [Command.label ~pos lab pt; draw_pic pic]
