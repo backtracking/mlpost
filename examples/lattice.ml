c4728861a904b70d3fd6e071f32f1b7957e33cad
Jean-Christophe Filliatre <filliatr@lri.fr>
2008-07-24 16:11:30 +0000

    il y a comme un pbm...

--- examples/lattice.ml
+++ examples/lattice.ml
@@ -10,12 +10,24 @@ open Helpers
 let dx = bp 4.
 let dy = bp 6.
 
-let node s = make (draw_box (Box.ellipse Point.origin (Picture.tex s)))
+let node s = 
+  let s = "\\rule[-0.1em]{0in}{0.8em}" ^ s in
+  Box.ellipse Point.origin (Picture.tex s)
+
+let pic b = make (draw_box b)
 
 let draw_boxes = List.map (draw_box ~boxed:false)
 
+let cross_arrows b l = seq (List.map (box_arrow b) l)
+
 let fig =
-  let l1 = Box.halign ~dx (List.map node ["ab"; "ac"; "bc"]) in
-  let l2 = Box.halign ~dx (List.map node ["a"; "b"; "c"]) in
-  let pl = List.map (fun l -> make (seq (draw_boxes l))) [l1; l2] in
-  draw_boxes (Box.valign ~dy pl)
+  let abc = node "abc" in
+  let ab = node "ab" and ac = node "ac" and bc = node "bc" in
+  let a = node "a" and b = node "b" and c = node "c" in
+  let l0 = Box.halign ~dx (List.map pic [abc]) in
+  let l1 = Box.halign ~dx (List.map pic [ab; ac; bc]) in
+  let l2 = Box.halign ~dx (List.map pic [a; b; c]) in
+  let pl = List.map (fun l -> make (seq (draw_boxes l))) [l0; l1; l2] in
+  [seq (draw_boxes (Box.valign ~dy pl));
+   cross_arrows abc [ab; ac; bc];
+   cross_arrows ab [a; b]; cross_arrows ac [a; c]; cross_arrows bc [b; c]]
