8875b93c8a66c2e9488b4e9a7777a1a0a0692866
Romain Bardou <bardou@lri.fr>
2008-09-12 14:38:35 +0000

    Makefile + ExtArrow

--- multi-examples/extarrows.ml
+++ multi-examples/extarrows.ml
@@ -15,6 +15,12 @@ let right = Path.Vec Point.right
 
 let double_headed = ExtArrow.add_foot ExtArrow.simple
 
+let multiple_headed =
+  ExtArrow.add_belt ~point: 0.
+    (ExtArrow.add_belt ~point: 0.25
+       (ExtArrow.add_belt ~point: 0.5
+          (ExtArrow.add_belt ~point: 0.75 ExtArrow.simple)))
+
 let () =
   emit [ExtArrow.draw2 a d];
   emit [ExtArrow.draw2 b c];
@@ -28,4 +34,7 @@ let () =
   (* Some double-headed arrows *)
   emit [ExtArrow.draw2 ~kind: double_headed a d];
   emit [ExtArrow.draw2 ~kind: double_headed ~outd: right c b];
+  (* Some multiple-headed arrows *)
+  emit [ExtArrow.draw2 ~kind: multiple_headed a d];
+  emit [ExtArrow.draw2 ~kind: multiple_headed ~outd: right c b];
   dump ()
