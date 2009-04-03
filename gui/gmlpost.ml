40139d549d3f84aef65a4117e4f15421fefe7d47
Julien Robert <jrobert@lri.fr>
2009-04-03 11:16:46 +0000

    a

--- gui/gmlpost.ml
+++ gui/gmlpost.ml
@@ -1,6 +1,7 @@
 open GMain
 open GdkKeysyms
 open Glexer
+open Format
 
 let () = ignore (GtkMain.Main.init ())
 
@@ -9,6 +10,7 @@ let file = Sys.argv.(1)
 let elements = Glexer.read_file file
 let () = Format.printf "%d elements@." (List.length elements)
 
+let spins = ref []
 
 let string_of_dim = function
   |Pt -> "pt"
@@ -22,9 +24,10 @@ let left_part_lign vbox vbox2 vbox3 s n d s' =
   GMisc.label ~text:(string_of_dim d) ~packing:vbox3#add ();      
   let sb = GEdit.spin_button ~packing:vbox2#add ~digits:2 ~numeric:true ~wrap:true ()
   in
-  sb#adjustment#set_bounds ~lower:(-200.) ~upper:200. ~step_incr:0.01 ();
-  sb#set_value n;
-  ()
+    sb#adjustment#set_bounds ~lower:(-200.) ~upper:200. ~step_incr:0.01 ();
+    sb#set_value n;
+    spins := sb::!spins;
+    ()
 
 let left_part vbox vbox2 vbox3 = function
   | Num (s,(n,d)) ->
@@ -34,6 +37,25 @@ let left_part vbox vbox2 vbox3 = function
       let s = String.sub s 1 ((String.length s )-2) in
       left_part_lign vbox vbox2 vbox3 s n1 d1 " xpart :" ;
       left_part_lign vbox vbox2 vbox3 s n2 d2 " ypart :" 
+  
+let rec go_string fmt = function
+  |[],_|_,[] -> ()
+  | Num (s,(_,d))::r,sp::sps ->
+      let n = sp#value in
+	fprintf fmt "num %s %f %s \n" s n (string_of_dim d);
+	go_string fmt (r,sps)
+  | Point (s,(_,d1),(_,d2))::r,sp1::sp2::sps ->
+      let n1 = sp1#value in
+      let n2 = sp2#value in
+	fprintf fmt "point %s %f %s , %f %s \n" s n1 (string_of_dim d1) n2 (string_of_dim d2);
+	go_string fmt (r,sps)
+	  
+let quit ()= 
+  let f = open_out "result" in
+  let fmt = formatter_of_out_channel f in
+    go_string fmt (elements,!spins);
+    fprintf fmt "@?";
+    close_out f
 
 (* *)
 let main () =
@@ -50,13 +72,17 @@ let main () =
   (* File menu *)
   let factory = new GMenu.factory file_menu ~accel_group in
   factory#add_item "Quit" ~key:_Q ~callback: Main.quit;
-  window#connect#destroy ~callback:Main.quit;
+    
+  window#connect#destroy ~callback:quit ;
+  window#connect#destroy ~callback:Main.quit ;
   window#add_accel_group accel_group;
 
   let hbox = GPack.hbox ~spacing:10 ~packing:vb#add () in
   let scrolled_window = GBin.scrolled_window  ~width:350
     ~border_width: 10 ~hpolicy: `AUTOMATIC ~packing: hbox#add ()
   in
+  (* let button = GButton.button ~use_mnemonic:true ~label:"_Coucou" ~packing:(hbox#pack ~padding:5) () in *)
+(*     button#connect#clicked ~callback:quit; *)
   let hbox2 = GPack.hbox ~spacing:10 ~packing:scrolled_window#add_with_viewport () in
   let frame = GBin.frame ~shadow_type:`IN ~packing:hbox#add () in
   let canvas = GnoCanvas.canvas ~width:500 ~height:500 ~packing:frame#add () in
@@ -64,10 +90,15 @@ let main () =
   let vbox2 = GPack.vbox ~spacing:10 ~packing:hbox2#add () in
   let vbox3 = GPack.vbox ~spacing:10 ~packing:hbox2#add () in
   
-  List.iter (left_part vbox vbox2 vbox3) elements;
-  
-  window#show ();
-  Main.main ()
+    List.iter (left_part vbox vbox2 vbox3) elements;
+    Format.printf "%d spins@." (List.length !spins);
+    spins := List.rev !spins;
+    Format.printf "%d spins@." (List.length !spins);
+    
+    quit ();
+    
+    window#show ();
+    Main.main ()
 ;;
 
 main ()
