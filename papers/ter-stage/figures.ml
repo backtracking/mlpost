42ec3830a5a02215e71e05d1d455f71d12df1cf6
Julien Robert <jrobert@lri.fr>
2009-04-24 15:26:21 +0000

    a

--- papers/ter-stage/figures.ml
+++ papers/ter-stage/figures.ml
@@ -17,8 +17,19 @@ let simple_block =
   let b = Box.hblock ~pos:`Bot [Box.tex "a"; Box.tex "b"; Box.tex "D"] in
   Box.draw b
 
-let () = Metapost.emit "simple_block" simple_block
+(* schema de l'interface *)
+let interface = 
+  let dx = bp 5. and dy = bp 5. in
+  let tex' = tex ~style:RoundRect ~dx ~dy in
+  let tex = tex' ~stroke:(Some Color.black) in
+  let box name = box ~stroke:None ~dx:(mm 2.) ~name in
+  let fml = box "fml" (tex "figure.ml") in
+  let fedit = box "fedit" (tex "figure.edit") in
+  let png = box "png" (tex "figure.png") in
+  nop
 
+let () = Metapost.emit "simple_block" simple_block
+let () = Metapost.emit "interface" interface
 
 (*
 Local Variables: 
