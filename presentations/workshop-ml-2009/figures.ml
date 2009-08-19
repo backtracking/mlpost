ad5e8c813638c3348ba11d60dbf512fff9001d02
Johannes Kanig <johannes.kanig@lri.fr>
2009-08-19 14:48:18 +0000

    koda-ruskey

--- presentations/workshop-ml-2009/figures.ml
+++ presentations/workshop-ml-2009/figures.ml
@@ -57,11 +57,110 @@ let diag =
   let arrow x y = box_arrow ~sep:(bp 5.) (sub x v) (sub y v) in
   draw v ++ arrow a b ++ arrow ab c 
 
+(* Koda-Ruskey *)
+
+open Command
+open Num
+open Path
+open Box
+open Tree
+
+type tree  = Node of int * forest 
+and forest = tree list
+
+type color = White | Black
+
+let bits = Array.create 100 White
+
+let rec enum_forest k = function
+  | [] ->
+      k ()
+  | Node (i, f') :: f ->
+      let k () = enum_forest k f in
+      if bits.(i) = White then begin
+	k (); bits.(i) <- Black; enum_forest k f'
+      end else begin
+	enum_forest k f'; bits.(i) <- White; k ()
+      end
+
+
+let leaf i = Node (i, [])
+let t1 = Node (0, [leaf 1])
+let t2 = Node (2, [leaf 3; leaf 4])
+let f0 = [t1; t2]
+
+let draw1 f0 =
+  let white = circle (empty ()) in
+  let black = circle ~fill:Color.black (empty ()) in
+  let node = node ~arrow_style:Undirected ~ls:(bp 10.) ~sep:(bp 2.) in
+  let rec make_tree = function
+    | [] ->
+	[]
+    | Node (i, sons) :: f ->
+	(node (if bits.(i) = White then white else black) (make_tree sons)) ::
+         make_tree f
+  in
+  hbox ~padding:(bp 5.) (List.map to_box (make_tree f0))
+
+(* let () = Metapost.emit "f0" (Box.draw(draw1 f0)) *)
+
+let gray f =
+  Array.fill bits 0 100 White;
+  let boxes = ref [] in
+  let k () = boxes := draw1 f :: !boxes in
+  enum_forest k f;
+  hbox ~padding:(bp 10.) (List.rev !boxes)
+
+let box_arrow = Helpers.box_arrow ~sep:(bp 10.)
+
+let smart_box_arrow ol a b =
+  let p = smart_path ~style:jLine ol (ctr a) (ctr b) in
+  let p = cut_after (bpath b) (cut_before (bpath a) p) in
+  let p = strip (bp 10.) p in
+  Arrow.simple p
+
+let gray_grid w f =
+  Array.fill bits 0 100 White;
+  let boxes = ref [[]] in
+  let k () = 
+    let b = draw1 f in
+    match !boxes with
+      | l :: _ when List.length l = w -> boxes := [b] :: !boxes
+      | l :: r -> boxes := (b :: l) :: r
+      | [] -> assert false
+  in
+  enum_forest k f;
+  let rec reverse = function
+    | [] -> []
+    | [x] -> [List.rev x]
+    | x :: y :: l -> List.rev x :: y :: reverse l 
+  in
+  let boxes = reverse (List.rev !boxes) in
+  let h = List.length boxes in
+  let b = gridl ~hpadding:(bp 20.) ~vpadding:(bp 10.) ~stroke:None boxes in
+  let sub i j = nth j (nth i b) in
+  Box.draw b ++
+  iter 0 (h-1) (fun i -> iter 0 (w-2) (fun j ->
+    if i mod 2 = 0 then
+      box_arrow (sub i j) (sub i (j+1))
+    else
+      box_arrow (sub i (j+1)) (sub i j)
+  )) ++
+  iter 0 (h-2) (fun i ->
+    if i mod 2 = 0 then
+      smart_box_arrow [Rightn (bp 25.); Down; Left] 
+	(sub i (w-1)) (sub (i+1) (w-1))
+    else
+      smart_box_arrow [Leftn (bp 25.); Down; Right] 
+	(sub i 0) (sub (i+1) 0)
+  )
+
 let _ = 
   List.iter (fun (name,fig) -> Metapost.emit name fig)
   [ 
     "fibtree", fibtree;
     "hist", hist;
     "diag", diag;
+    "gray_f0", gray_grid 5 f0;
 (*     "tree10", tree10; *)
   ]
