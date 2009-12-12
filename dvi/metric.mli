a5c855c2e9ff6a8ee03e1417d7a22f7ec117bdd2
Johannes Kanig <johannes.kanig@lri.fr>
2009-12-12 16:32:51 +0000

    more accessors for metrics

--- dvi/metric.mli
+++ dvi/metric.mli
@@ -12,3 +12,28 @@ val char_depth : t -> int -> float
 (* same as [char_width], but for character depth *)
 val char_italic : t -> int -> float
 (* same as [char_width], but for italic correction of the character *)
+
+
+val slant : t -> float
+(** is the amount of italic slant, which is used to help position accents. For
+  example, slant=.25 means that when you go up one unit, you also go .25 units
+  to the right. *)
+
+val space : t -> float
+(** is the normal spacing between words in text. Note that character " " in the
+   font need not have anything to do with blank spaces. *)
+
+val space_stretch : t -> float
+(** is the amount of glue stretching between words. *)
+
+val space_shrink : t -> float
+(** is the amount of glue shrinking between words. *)
+
+val x_height : t -> float
+(** is the height of letters for which accents don't have to be raised or
+   lowered. *)
+
+val quad : t -> float
+(** is the size of one em in the font. *)
+val extra_space : t -> float
+(** is the amount added to [space] at the ends of sentences. *)
