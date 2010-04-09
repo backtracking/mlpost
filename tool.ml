1f147666fe81531d726ea556bcf4fb8fab2870bd
Francois Bobot <bobot@bhuta.lri.fr>
2010-04-09 16:42:45 +0200

    unix comme librairie

--- tool.ml
+++ tool.ml
@@ -36,7 +36,7 @@ let not_bitstring = Version.not_bitstring
 
 let used_libs =
   (* put libraries in correct order here *)
-  let acc = [] in
+  let acc = ["unix"] in
   let acc = if not_cairo then acc else "cairo"::acc in
   let acc = if not_bitstring then acc else "bitstring"::acc in
   let acc = "mlpost"::acc in
@@ -208,7 +208,7 @@ let ocaml args =
         execute ~outv:true ("ocaml" ^ String.concat " " args)
     | _ -> *)
   let s = get_exec_name !compile_name in
-  let cmd = "ocamlc" ^ String.concat " " args ^ " -o " ^ s in
+  let cmd = "ocamlc " ^ String.concat " " args ^ " -o " ^ s in
   command ~outv:true cmd;
   execute ~outv:true s;
   match !compile_name with
