4ed00cb714f3f93111a20f6c3cf1b4fdfe6c9861
Johannes Kanig <johannes.kanig@lri.fr>
2010-08-25 17:32:29 +0200

    names for assert statements

--- testsuite/test_file.ml
+++ testsuite/test_file.ml
@@ -4,9 +4,9 @@ let test_from_string =
   Test.mk ~name:"from_string" (fun () ->
     let s = "toto.mps" in
     let f = File.from_string s in
-    Assert.eq (File.to_string f) "toto.mps";
+    Assert.eq ~s:"toto.mps" (File.to_string f) "toto.mps";
     let p = File.set_ext f "pdf" in
-    Assert.eq (File.to_string p) "toto.pdf")
+    Assert.eq ~s:"toto.pdf" (File.to_string p) "toto.pdf")
 
 let tests =
   [ test_from_string;
