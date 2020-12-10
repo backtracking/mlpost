24777f07326382d6fddc0af27d0261c37c2ebbd2
François Bobot <francois.bobot@cea.fr>
2020-12-10 15:38:26 +0100

    Latex and required doesn't work because they are outside the directory

--- tools/mlpost_init.ml
+++ tools/mlpost_init.ml
@@ -27,22 +27,23 @@ let additional_options mode =
   | "png" -> ["-cairo"]
   | _ -> []
 
-let print_dune figures latex fmt =
-  let latex = match latex with None -> "" | Some s -> Fmt.str "-latex %S" s in
+let print_dune figures latex required fmt =
+  let dep fmt s = Fmt.pf fmt "%%{dep:%s}" (Filename.concat ".." s) in
   Fmt.pf fmt "(executables (names %a) (libraries mlpost mlpost.options))@\n@\n" Fmt.(list string) figures;
   List.iter (fun fig ->
       List.iter (fun mode ->
-      Fmt.pf fmt "(rule (targets %S) (alias %s) (action (run %S -%s %s %a)) (mode promote-until-clean))"
+      Fmt.pf fmt "(rule (targets %S) (alias %s) (action (run %S -%s %a %a %a)) (mode promote-until-clean))"
         (fig^"."^mode)
         mode
         ("./"^fig^".exe")
         mode
-        latex
+        Fmt.(option (any "-latex " ++ dep)) latex
+        Fmt.(list ~sep:sp (any "-required " ++ dep)) required
         Fmt.(list string) (additional_options mode)
         ) modes) figures;
   ()
 
-let run figures latex outdir =
+let run figures latex required outdir =
   Unix.mkdir outdir 0o770;
   writef print_dune_project (Filename.concat outdir "dune-project");
   List.iter
@@ -50,7 +51,7 @@ let run figures latex outdir =
         (print_fig_template fig)
         (Filename.concat outdir (fig ^ ".ml")))
     figures;
-  writef (print_dune figures latex) (Filename.concat outdir "dune");
+  writef (print_dune figures latex required) (Filename.concat outdir "dune");
   writef print_common_template (Filename.concat outdir "common_for_figures.ml");
   0
 
@@ -60,8 +61,10 @@ let cmd =
                      info [] ~docv:"FIG" ~doc:"mlpost figures to initialize") in
   let latex = Arg.(value & opt (some string) None &
                      info ["latex"] ~docv:"TEX" ~doc:"latex file to include") in
+  let required = Arg.(value & opt_all string [] &
+                     info ["required"] ~docv:"TEX" ~doc:"additional required files") in
   let outdir = Arg.(value & opt string "mlpost_figures" &
                      info ["dir"] ~docv:"DIR" ~doc:"directory to initialize") in
-  Term.(const run $ figures $ latex $ outdir)
+  Term.(const run $ figures $ latex $ required $ outdir)
 
 let () = exit (Cmdliner.Cmd.(eval' (v (info "mlpost-init") cmd)))
